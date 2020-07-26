71d3eaf8be579ea3c9958973b2bdd5d5afaf9ed0
Anand <anand.sg@fleximc.com>
2020-07-26 22:52:43 +0530

    Updates

---
+++ precache-manifest.15301de16013b9c0c59568752898fea2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc56a0535abf1ff3d33cb5424eae065",
+    "url": "/Task_teksystems/index.html"
+  },
+  {
+    "revision": "c542f21b96364f06a8cb",
+    "url": "/Task_teksystems/static/css/main.29b0bf7f.chunk.css"
+  },
+  {
+    "revision": "260bb6f87ad6c9aae240",
+    "url": "/Task_teksystems/static/js/2.5352df3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Task_teksystems/static/js/2.5352df3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c542f21b96364f06a8cb",
+    "url": "/Task_teksystems/static/js/main.1401e191.chunk.js"
+  },
+  {
+    "revision": "e7bc76d302ca2718f76e",
+    "url": "/Task_teksystems/static/js/runtime-main.014dea42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Task_teksystems/static/media/logo.5d5d9eef.svg"
+  }
+]);
